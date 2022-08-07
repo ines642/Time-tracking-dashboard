96dff40e13c0e4b63206cdb81f59440a088fdf3e
ines642 <ineschicha4@gmail.com>
2022-08-07 23:25:46 +0100

    1st commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const add =document.querySelectorAll('.add')
+const dur =document.querySelectorAll('.duration')
+const d =document.querySelector('.daily')
+const w =document.querySelector('.weekly')
+const m =document.querySelector('.monthly') 
+//confirm selection
+console.log(d ,m , w );
+for (let i = 0; i < add.length; i++) {
+    console.log(add[i]);
+    console.log(dur[i]); }
+    

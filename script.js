c632aa26deabdb902324d0490150c9e735120df4
ines642 <ineschicha4@gmail.com>
2022-08-08 02:14:47 +0100

    add js code

--- script.js
+++ script.js
@@ -1,11 +1,49 @@
 const add =document.querySelectorAll('.add')
 const dur =document.querySelectorAll('.duration')
+const pre =document.querySelectorAll('.previous')
 const d =document.querySelector('.daily')
 const w =document.querySelector('.weekly')
-const m =document.querySelector('.monthly') 
+const m =document.querySelector('.monthly')
+const preh =document.querySelectorAll('.prehours')
 //confirm selection
 console.log(d ,m , w );
 for (let i = 0; i < add.length; i++) {
-    console.log(add[i]);
-    console.log(dur[i]); }
+    console.log(add[i] , dur[i] , pre[i] ,preh[i]);
+}
+// change pre and der based on time clicked
+/*1st way :
+make each activity an array
+const play = [5 , 7 , 32 , 36 , 103 ,128 ]
+
+*/
+let day=[5 , 1 , 0 , 1 , 1 , 0]
+let preday=[7 , 2 , 1 , 1 ,3 ,1]
+let week =[32, 10 , 4 , 4 , 5 , 2]
+let preweek=[36, 8 , 7 , 5 , 10 , 2]
+let month=[103 , 23 , 13 , 11 , 21 ,7]
+let premonth=[128,29,19,18,23,11]
+
+d.addEventListener('click' , ()=>{
+for (let e = 0 ; e< pre.length ; e++) {
+    console.log(e);
+    dur[e].innerText=day[e]+'hrs'
+    pre[e].innerText='Last day '+ preday[e] +'hrs'
     
+}
+})
+w.addEventListener('click' , ()=>{
+    for (let e = 0 ; e< pre.length ; e++) {
+        console.log(e);
+        dur[e].innerText=week[e]+'hrs'
+        pre[e].innerText='Last week '+ preweek[e]+'hrs'
+        
+    }
+    })
+    m.addEventListener('click' , ()=>{
+        for (let e = 0 ; e< pre.length ; e++) {
+            console.log(e);
+            dur[e].innerText=month[e]+'hrs'
+            pre[e].innerText='Last month '+ premonth[e]+'hrs'
+            
+        }
+        })+'hrs'
